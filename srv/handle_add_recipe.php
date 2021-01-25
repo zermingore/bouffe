1bd8dad8924697a55300305c46b569f78c2057a6
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2021-01-25 07:01:40 +0100

    [Clean] Indenting PHP code after PHP tag

--- srv/handle_add_recipe.php
+++ srv/handle_add_recipe.php
@@ -21,158 +21,158 @@ Notes: <?php echo $_POST["notes"]?><br/>
 <hr/>
 
 <?php
-$db_file = "../db/db";
-$db = new SQLite3("$db_file");
+  $db_file = "../db/db";
+  $db = new SQLite3("$db_file");
 
-$lg = 1;
-if (isset($_GET['lg']))
-{
-  $lg = $_GET['lg'];
-  # TODO check language_table['$_GET['lg']'] exists
-}
-
-
-// Insert the recipe name only if it does not exist yet
-$query = "SELECT id FROM words WHERE name='" . $_POST['name'] . "';";
-$name_id = $db->querySingle($query);
-if (empty($name_id))
-{
-  $query = "INSERT INTO words('name') VALUES('" . $_POST['name'] . "');";
-  $db->querySingle($query);
-  $name_id=$db->lastInsertRowID();
-}
-
-
-// Insert the recipe summary only if it does not exist yet
-$query = "SELECT id FROM words WHERE name='" . $_POST['summary'] . "';";
-$summary_id = $db->querySingle($query);
-if (empty($summary_id))
-{
-  $query = "INSERT INTO words('name') VALUES('" . $_POST['summary'] . "');";
-  $db->querySingle($query);
-  $summary_id=$db->lastInsertRowID();
-}
-
-// echo "<pre>"; print_r($_POST); echo "</pre>";
-
-$query = "INSERT INTO recipes(
-  id_word,
-  summary,
-  time_total, time_preparation, time_crafting, time_backing,
-  quantity, difficulty, annoyance, threads) VALUES("
-  . "'" . $name_id . "', '" . $summary_id . "', '"
-  . $_POST['time_total'] . "', '" . $_POST['time_preparation'] . "', '"
-  . $_POST['time_crafting'] . "', '" . $_POST['time_backing'] . "', '"
-  . $_POST['difficulty'] . "', '" . $_POST['annoyance'] . "', '" . $_POST['threads'] . "', '"
-  . $_POST['quantity']
-  . "');";
-$db->query($query);
-$id_recipe = $db->lastInsertRowID();
-
-
-
-
-echo "Ingredients:<br/>";
-
-$db_ingredients = $db->query("SELECT * FROM words WHERE id IN (SELECT id FROM ingredients)");
-
-// TODO handle translations (searching by name...)
-
-// 100: ingredients max number should be 30
-for ($i = 1; $i <= 100; $i++)
-{
-  if (!isset($_POST["ingredient_" . $i . "_name"]))
+  $lg = 1;
+  if (isset($_GET['lg']))
   {
-    // echo "Found " . ($i - 1) . " ingredients\n";
-    return;
-  }
-
-  if (!isset($_POST["ingredient_" . $i . "_qty"]) || !isset($_POST["ingredient_" . $i . "_qty_unit"]))
-  {
-    echo "Ill-Formed ingredient:<br/>";
-    echo "<pre>"; print_r($_POST); echo "</pre>";
-    return;
+    $lg = $_GET['lg'];
+    # TODO check language_table['$_GET['lg']'] exists
   }
 
 
-  $ingredient_name = $_POST["ingredient_" . $i . "_name"];
-  $ingredient_found = 0;
-  $db_ingredients->reset();
-  while ($res = $db_ingredients->fetchArray())
+  // Insert the recipe name only if it does not exist yet
+  $query = "SELECT id FROM words WHERE name='" . $_POST['name'] . "';";
+  $name_id = $db->querySingle($query);
+  if (empty($name_id))
   {
-    if ($res['name'] == $ingredient_name)
+    $query = "INSERT INTO words('name') VALUES('" . $_POST['name'] . "');";
+    $db->querySingle($query);
+    $name_id=$db->lastInsertRowID();
+  }
+
+
+  // Insert the recipe summary only if it does not exist yet
+  $query = "SELECT id FROM words WHERE name='" . $_POST['summary'] . "';";
+  $summary_id = $db->querySingle($query);
+  if (empty($summary_id))
+  {
+    $query = "INSERT INTO words('name') VALUES('" . $_POST['summary'] . "');";
+    $db->querySingle($query);
+    $summary_id=$db->lastInsertRowID();
+  }
+
+  // echo "<pre>"; print_r($_POST); echo "</pre>";
+
+  $query = "INSERT INTO recipes(
+    id_word,
+    summary,
+    time_total, time_preparation, time_crafting, time_backing,
+    quantity, difficulty, annoyance, threads) VALUES("
+    . "'" . $name_id . "', '" . $summary_id . "', '"
+    . $_POST['time_total'] . "', '" . $_POST['time_preparation'] . "', '"
+    . $_POST['time_crafting'] . "', '" . $_POST['time_backing'] . "', '"
+    . $_POST['difficulty'] . "', '" . $_POST['annoyance'] . "', '" . $_POST['threads'] . "', '"
+    . $_POST['quantity']
+    . "');";
+  $db->query($query);
+  $id_recipe = $db->lastInsertRowID();
+
+
+
+
+  echo "Ingredients:<br/>";
+
+  $db_ingredients = $db->query("SELECT * FROM words WHERE id IN (SELECT id FROM ingredients)");
+
+  // TODO handle translations (searching by name...)
+
+  // 100: ingredients max number should be 30
+  for ($i = 1; $i <= 100; $i++)
+  {
+    if (!isset($_POST["ingredient_" . $i . "_name"]))
     {
-      // echo $_POST["ingredient_" . $i . "_name"] . " already in DB!<br/>";
+      // echo "Found " . ($i - 1) . " ingredients\n";
+      return;
+    }
 
-      $ingredient_found = 1;
-      break;
+    if (!isset($_POST["ingredient_" . $i . "_qty"]) || !isset($_POST["ingredient_" . $i . "_qty_unit"]))
+    {
+      echo "Ill-Formed ingredient:<br/>";
+      echo "<pre>"; print_r($_POST); echo "</pre>";
+      return;
+    }
+
+
+    $ingredient_name = $_POST["ingredient_" . $i . "_name"];
+    $ingredient_found = 0;
+    $db_ingredients->reset();
+    while ($res = $db_ingredients->fetchArray())
+    {
+      if ($res['name'] == $ingredient_name)
+      {
+        // echo $_POST["ingredient_" . $i . "_name"] . " already in DB!<br/>";
+
+        $ingredient_found = 1;
+        break;
+      }
+    }
+
+    // Insert the ingredient name only if it does not exist yet
+    if (!$ingredient_found)
+    {
+      $query = "INSERT INTO words('name') VALUES('" . $ingredient_name . "');";
+      $db->querySingle($query);
+      $query = "INSERT INTO ingredients('id') VALUES('" . $db->lastInsertRowID() . "');";
+      $db->querySingle($query);
+    }
+
+
+
+    // Fetch the ingredient id
+    $ingredient_id = $db->querySingle(
+      "SELECT * FROM ingredients WHERE id IN (SELECT id FROM words WHERE name='" . $ingredient_name . "')");
+
+    // Fetch the quantity unit id
+    $quantity_unit_id = $db->querySingle(
+      "SELECT * FROM quantities WHERE id_word IN (SELECT id FROM words WHERE name='" . $_POST["ingredient_" . $i . "_qty_unit"] . "')");
+
+    // Add the requirement
+    $query = "INSERT INTO requirements('id_recipe', 'id_ingredient', 'quantity', 'id_quantity_unit') VALUES('"
+      . $id_recipe . "','" . $ingredient_id . "', '" . $_POST["ingredient_" . $i . "_qty"] . "', '" . $quantity_unit_id . "');";
+    $db->query($query);
+
+
+    if ($_POST["ingredient_" . $i . "_qty_unit"] != "-")
+    {
+      echo $ingredient_name . " (". $_POST["ingredient_" . $i . "_qty"] . " ". $_POST["ingredient_" . $i . "_qty_unit"] . ")<br/>";
+    }
+    else
+    {
+      echo $_POST["ingredient_" . $i . "_qty"] . " " . $ingredient_name . "<br/>";
     }
   }
 
-  // Insert the ingredient name only if it does not exist yet
-  if (!$ingredient_found)
+
+
+  $steps = preg_split('/\n|\r/', $_POST['steps'], -1, PREG_SPLIT_NO_EMPTY);
+  $i = 1;
+  foreach ($steps as $step)
   {
-    $query = "INSERT INTO words('name') VALUES('" . $ingredient_name . "');";
+    $query = "INSERT INTO words('name') VALUES('" . $step . "');";
     $db->querySingle($query);
-    $query = "INSERT INTO ingredients('id') VALUES('" . $db->lastInsertRowID() . "');";
+
+    $query = "INSERT INTO steps('id_language', 'id_recipe', 'num', 'description') VALUES('"
+      . $lg . "', '" . $id_recipe
+      . "', '" . $i . "', '" . $db->lastInsertRowID() . "');";
+    $db->querySingle($query);
+
+    ++$i;
+  }
+
+
+  $notes = preg_split('/\n|\r/', $_POST['notes'], -1, PREG_SPLIT_NO_EMPTY);
+  foreach ($notes as $note)
+  {
+    $query = "INSERT INTO words('name') VALUES('" . $note . "');";
+    $db->querySingle($query);
+
+    $query = "INSERT INTO notes('id_language', 'id_recipe', 'description') VALUES('"
+      . $lg . "', '" . $id_recipe
+      . "', '" . $db->lastInsertRowID() . "');";
     $db->querySingle($query);
   }
-
-
-
-  // Fetch the ingredient id
-  $ingredient_id = $db->querySingle(
-    "SELECT * FROM ingredients WHERE id IN (SELECT id FROM words WHERE name='" . $ingredient_name . "')");
-
-  // Fetch the quantity unit id
-  $quantity_unit_id = $db->querySingle(
-    "SELECT * FROM quantities WHERE id_word IN (SELECT id FROM words WHERE name='" . $_POST["ingredient_" . $i . "_qty_unit"] . "')");
-
-  // Add the requirement
-  $query = "INSERT INTO requirements('id_recipe', 'id_ingredient', 'quantity', 'id_quantity_unit') VALUES('"
-    . $id_recipe . "','" . $ingredient_id . "', '" . $_POST["ingredient_" . $i . "_qty"] . "', '" . $quantity_unit_id . "');";
-  $db->query($query);
-
-
-  if ($_POST["ingredient_" . $i . "_qty_unit"] != "-")
-  {
-    echo $ingredient_name . " (". $_POST["ingredient_" . $i . "_qty"] . " ". $_POST["ingredient_" . $i . "_qty_unit"] . ")<br/>";
-  }
-  else
-  {
-    echo $_POST["ingredient_" . $i . "_qty"] . " " . $ingredient_name . "<br/>";
-  }
-}
-
-
-
-$steps = preg_split('/\n|\r/', $_POST['steps'], -1, PREG_SPLIT_NO_EMPTY);
-$i = 1;
-foreach ($steps as $step)
-{
-  $query = "INSERT INTO words('name') VALUES('" . $step . "');";
-  $db->querySingle($query);
-
-  $query = "INSERT INTO steps('id_language', 'id_recipe', 'num', 'description') VALUES('"
-    . $lg . "', '" . $id_recipe
-    . "', '" . $i . "', '" . $db->lastInsertRowID() . "');";
-  $db->querySingle($query);
-
-  ++$i;
-}
-
-
-$notes = preg_split('/\n|\r/', $_POST['notes'], -1, PREG_SPLIT_NO_EMPTY);
-foreach ($notes as $note)
-{
-  $query = "INSERT INTO words('name') VALUES('" . $note . "');";
-  $db->querySingle($query);
-
-  $query = "INSERT INTO notes('id_language', 'id_recipe', 'description') VALUES('"
-    . $lg . "', '" . $id_recipe
-    . "', '" . $db->lastInsertRowID() . "');";
-  $db->querySingle($query);
-}
 
 ?>
 
--- srv/helper.php
+++ srv/helper.php
@@ -1,48 +1,48 @@
 <?php
 
-class Helper
-{
-  public function __construct(string $db_path)
+  class Helper
   {
-    $this->db = new SQLite3($db_path);
+    public function __construct(string $db_path)
+    {
+      $this->db = new SQLite3($db_path);
+    }
+
+
+    public function fetchWord(string $word)
+    {
+      $language_id = $_SESSION["language"];
+
+      // Default language
+      if ($language_id == "1")
+      {
+        return $word;
+      }
+
+      // Sanity check
+      if ($language_id != "2" && $language_id != "3")
+      {
+        echo "Invalid language " . $language_id . "<br/>";
+        return "___";
+      }
+
+      // Fetch the appropriate translation
+      $query = "SELECT name from translations where id_language='"
+        . $language_id . "' and id_word = "
+        . "(SELECT id FROM words WHERE name='" . $word . "')";
+
+      // echo $query;
+      $text =  $this->db->querySingle($query);
+
+      if ($text == "")
+      {
+        echo "ERROR: missing word<br/>";
+        return $word;
+      }
+      return $text;
+    }
+
+
+    private $db;
   }
-
-
-  public function fetchWord(string $word)
-  {
-    $language_id = $_SESSION["language"];
-
-    // Default language
-    if ($language_id == "1")
-    {
-      return $word;
-    }
-
-    // Sanity check
-    if ($language_id != "2" && $language_id != "3")
-    {
-      echo "Invalid language " . $language_id . "<br/>";
-      return "___";
-    }
-
-    // Fetch the appropriate translation
-    $query = "SELECT name from translations where id_language='"
-      . $language_id . "' and id_word = "
-      . "(SELECT id FROM words WHERE name='" . $word . "')";
-
-    // echo $query;
-    $text =  $this->db->querySingle($query);
-
-    if ($text == "")
-    {
-      echo "ERROR: missing word<br/>";
-      return $word;
-    }
-    return $text;
-  }
-
-
-  private $db;
-}
 
 ?>
